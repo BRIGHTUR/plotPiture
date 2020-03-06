36c0c9e255a5816214797ab3bd8468b3f3549c27
BRIGHTUR <849286047@qq.com>
2020-03-06 15:34:40 +0800

    可以运行的

--- plotPiture/plotPicture.cpp
+++ plotPiture/plotPicture.cpp
@@ -4,7 +4,9 @@ using namespace cv;
 using namespace std;
 int main()
 {	
-	cv::OutputArray dispMat;
+	cv::Mat dispMat=imread("1.png");
+	cv::Mat dispMat1 = imread("1.png");
+	cv::Mat dispMat2 = imread("1.png");
 	cv::Point pt;
 	pt.x = 10;
 	pt.y = 10;
@@ -15,14 +17,16 @@ int main()
 	cv::Point pt2;
 	pt2.x = 0;
 	pt2.y = 0;
-	line(dispMat, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
+	line(dispMat1, pt1, pt2, CV_RGB(255, 0, 0), 1, 8, 0);
 	cv::Rect rect;
 	rect.x = 10;
 	rect.y = 10;
 	rect.width;
 	rect.height;
-	rectangle(dispMat, rect, CV_RGB(255, 0, 0), 1, 8, 0);
+	rectangle(dispMat2, rect, CV_RGB(255, 0, 0), 1, 8, 0);
+	imshow("dispmat1",dispMat);
+	imshow("dispmat2", dispMat1);
+	imshow("dispmat3", dispMat2);
 	waitKey(0);
 	system("pause");
-	line()
 }
